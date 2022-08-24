0e22f95dbf12c4222444d67ebf294dbd6281b7c3
KokabMedia <kokabmedia@gmail.com>
2022-08-24 20:39:25 +0200

    first commit

---
+++ src/main/java/com/kokab/CWM/CwmApplication.java
@@ -0,0 +1,13 @@
+package com.kokab.CWM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kokab/CWM/CwmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kokab.CWM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
